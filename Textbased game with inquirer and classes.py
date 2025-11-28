09fae8782dbe9d3bfba4ba2e2df9c5b252c690d3
Alvar Eklind <alvar.eklind@gmail.com>
2025-11-28 13:28:50 +0100

    add slow_text function and Charachter class with attack method

---
+++ Textbased game with inquirer and classes.py
@@ -0,0 +1,48 @@
+import random
+from InquirerPy import inquirer
+import time
+
+"""
+How to write inquirer
+choice = inquirer.select(
+    message="welcome",
+    choices = ["Start", "End"]
+).execute()
+"""
+
+def slow_text(message):
+    message += "\n"
+    for letter in message:
+        print(letter, end="", flush=True)
+        time.sleep(0.03)
+
+class Charachter:
+    #Initiate all varibles
+    def __init__(self, health, dmgLower, dmgHigher, base_dmg, healingLower, healingHigher, name):
+        self.health = health
+        self.dmgLower = dmgLower
+        self.dmgHigher = dmgHigher
+        self.base_dmg = base_dmg
+        self.healingLower = healingLower
+        self.healingHigher = healingHigher
+        self.name = name
+    
+    def attack(self, enemy_object, player_object):
+        #Calulate dmg
+        dmg = random.randint(self.dmgLower, self.dmgHigher)
+
+        #Calculate crit
+        crit = random.randint(1,20)
+
+        #Calculate total damage
+        total_dmg = self.base_dmg + dmg
+
+        #Deals dmg based on if critted or not
+        if crit == 11:
+            crit_dmg = total_dmg * 1.5
+            enemy_object.take_damage(crit_dmg, enemy_object, player_object)
+            slow_text(f"You got a crit and dealt {crit_dmg} to {enemy_object.name}")
+        else:
+            enemy_object.take_damage(total_dmg, enemy_object, player_object)
+            slow_text(f"{enemy_object.name} took {total_dmg} amount of dmg")
+        
