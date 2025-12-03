6b6250b72a718b041c1eaf830e341f626131aafc
Alvar Eklind <alvar.eklind@gmail.com>
2025-12-03 14:19:30 +0100

    add code for door ending

--- Textbased game with inquirer and classes.py
+++ Textbased game with inquirer and classes.py
@@ -224,3 +224,26 @@ def final_door_fight(player_object, enemy_object, enemies):
     slow_text(f"The evil mystery is now solved and legends of {player_object.getName()} was told for decades to come")
     slow_text("The end")
     credits()
+
+# Code for door ending
+def door_code(which_door, amount_of_enemies, enemy_object, player_object, enemies, items):
+    slow_text(f"You take the {which_door} door")
+    slow_text(f"There is {amount_of_enemies} in the room")
+    if amount_of_enemies == 1:
+        add_enemy(enemies, enemy_object.getName())
+        combat(enemies, enemy_object, player_object)
+        max_items(items, player_object)
+    elif amount_of_enemies == 2:
+        add_enemy(enemies, enemy_object.getName())
+        add_enemy(enemies, enemy_object.getName())
+        combat(enemies, enemy_object, player_object)
+        max_items(items, player_object)
+        max_items(items, player_object)
+    elif amount_of_enemies == 3:
+        add_enemy(enemies, enemy_object.getName())
+        add_enemy(enemies, enemy_object.getName())
+        add_enemy(enemies, enemy_object.getName())
+        combat(enemies, enemy_object, player_object)
+        max_items(items, player_object)
+        max_items(items, player_object)
+        max_items(items, player_object)
